f0840cd065069ed2a5c255f58df6f47e30a52f15
liuyujie <tydic230ldap>
2020-07-02 15:57:10 +0800

    test

---
+++ liuyujie-springcloud/eureka-server/src/main/java/com/liuyujie/test2.java
@@ -0,0 +1,11 @@
+package com.liuyujie;
+
+/**
+ * @author liuyishou
+ * @date 2020/7/2
+ */
+public class test2 {
+    public static void main(String[] args) {
+        System.out.println("222");
+    }
+}
