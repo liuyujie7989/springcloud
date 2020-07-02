ec642d8cf5ea0330b98f14ca7c8f0c5cd1ef62e7
liuyujie <tydic230ldap>
2020-07-02 15:57:10 +0800

    同步

---
+++ liuyujie-springcloud/eureka-server/src/main/java/com/liuyujie/test.java
@@ -0,0 +1,11 @@
+package com.liuyujie;
+
+/**
+ * @author liuyishou
+ * @date 2020/7/2
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("111");
+    }
+}
